3fb9ca3519c471b04c0f267cc5a4eaeb31ea3247
Alexandros Lekkas <alexandros.lekkas@outlook.com>
2024-09-09 12:04:57 +0300

    Templates deleted, no longer rendering, newapp SystemMessage

--- newapp.py
+++ newapp.py
@@ -5,10 +5,11 @@ from flask import Flask, request, jsonify
 from langchain_openai import ChatOpenAI
 from langchain_core.messages import HumanMessage, AIMessage, SystemMessage, trim_messages
 
-# Configuration loader
 def load_config(config_path='config.yaml'):
     with open(config_path, 'r') as file:
         return yaml.safe_load(file)
+
+app = Flask(__name__) 
 
 @app.route("/advice/product", methods=['POST'])
 def advice():
@@ -18,17 +19,23 @@ def advice():
     item_data = json.loads(item_data)
     
     messages = []
+    api_call = "none"
     
     if item_data:
         messages = {
             SystemMessage(content = (
-                
+                "You are an AI assistant who's role is to determine if an API call is needed based on a message."
+                "API calls are these options: estimate_shipping, none"
+                "When you determine the api_call you will return it as json"
+                "e.g { 'api_call': 'estimate_shipping'} <- you must only respond with this JSON"
+                "For example, if you get the json of an item data, and the user also says 'what is the estimated shipping to greece'?"
+                "In this case the API call will be estimate_shipping"
+                "If the user asks"
             ))
         }
-    else:
-        print("")
- 
-app = Flask(__name__) 
+        
+        
+
 if __name__ == '__main__':
     config = load_config()
     
