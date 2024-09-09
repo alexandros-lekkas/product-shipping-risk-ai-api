b1e549051347bdc05fa6c939e132fcdc877602b7
Alexandros Lekkas <alexandros.lekkas@outlook.com>
2024-09-09 11:57:36 +0300

    Added config, changes to newapp

--- newapp.py
+++ newapp.py
@@ -1,11 +1,38 @@
+import json
+import os
+import yaml
 from flask import Flask, request, jsonify
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage, AIMessage, SystemMessage, trim_messages
 
-app = Flask(__name__)
+# Configuration loader
+def load_config(config_path='config.yaml'):
+    with open(config_path, 'r') as file:
+        return yaml.safe_load(file)
 
 @app.route("/advice/product", methods=['POST'])
 def advice():
-    data = request.json
+    user_input = request.form['user_input']
+    item_data = request.form['item_data']
     
-    if 'messages' not in session:
-        session['messages'] = []
-        print()
+    item_data = json.loads(item_data)
+    
+    messages = []
+    
+    if item_data:
+        messages = {
+            SystemMessage(content = (
+                
+            ))
+        }
+    else:
+        print("")
+ 
+app = Flask(__name__) 
+if __name__ == '__main__':
+    config = load_config()
+    
+    os.environ["OPENAI_API_KEY"] = os.getenv("OEPNAI_API_KEY")
+    model = ChatOpenAI(model=config['openai']['model'])
+    
+    app.run(config['debug'])
