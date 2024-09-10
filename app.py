b8b3b0a53f5ca58a10fdea64c0c264982656261b
Alexandros Lekkas <alexandros.lekkas@outlook.com>
2024-09-10 13:12:54 +0300

    Finally working, had to improve SQL code

--- app.py
+++ app.py
@@ -37,8 +37,16 @@ def invoke_model_simple(prompt, content):
     response = model.invoke(messages)
     return response
 
+# Initialize console
+console = Console()
+
+# Initialize config and FastAPI
 config = load_config()
 app = FastAPI(debug=config['debug'])
+
+# Initialize OpenAI model globally (accessible by all methods and API calls)
+os.environ['OPENAI_API_KEY'] = os.getenv('OPENAI_API_KEY')
+model = ChatOpenAI(model=config['openai']['model'])
 
 # Get advice for a product (could be risk, could be shipping)
 class ProductAdviceRequest(BaseModel):
@@ -53,18 +61,18 @@ def advice(request: ProductAdviceRequest):
     prompt = load_prompt('prompts/api_call_determination.txt')
     response = invoke_model_simple(prompt, f'User Message: {user_input}\nItem Data: {item_data}')
     api_call = parse_ai_response(response, 'api_call', 'none')
-    print('[bold green]Response (Categorization):[/bold green]',response.content)
+    console.print('[bold green]Response (Categorization):[/bold green]',response.content)
     
     # Seperate functionality (based on determined API call)
     if api_call == 'none': # None / No API Call
         prompt = load_prompt('prompts/no_call.txt')
         response = invoke_model_simple(prompt, f'User Message: {user_input}\nItem Data: {item_data}')
-        print('[bold green]Response (No Call):[/bold green]',response.content)
+        console.print('[bold green]Response (No Call):[/bold green]',response.content)
         return response.content
     elif api_call == 'estimate_shipping': # Estimate Shipping
         prompt = load_prompt('prompts/estimate_shipping_parameters.txt')
         response = invoke_model_simple(prompt, f'User Message: {user_input}\nItem Data: {item_data}')
-        print('[bold green]Response (Estimate Shipping):[/bold green]',response.content)
+        console.print('[bold green]Response (Estimate Shipping):[/bold green]',response.content)
 
         natural_language_error_message = 'Unfortunately I ran into some issues while attempting to estimate the shipping for this item. Is there anything else I can help with?'
                 
@@ -79,38 +87,39 @@ def advice(request: ProductAdviceRequest):
         keys = [country, weight_g, height_cm, length_cm, width_cm]
         for key in keys:
             if (key == ''):
-                print('[bold red]Issues:[/bold red]',natural_language_error_message)
+                console.print('[bold red]Issues:[/bold red]',natural_language_error_message)
                 return natural_language_error_message
         
         # Perform API call
         config = load_config()
         api_url = config['api_calls']['estimate_shipping']['url']
         payload = {
-            "country_name": country,
-            "weight_g": weight_g,
-            "height_cm": height_cm,
-            "width_cm": width_cm,
-            "length_cm": length_cm
+            "country": country,
+            "weightg": weight_g,
+            "heightcm": height_cm,
+            "widthcm": width_cm,
+            "lengthcm": length_cm
         }
-        response = requests.post(api_url, json=payload)
+        headers = {
+            'Content-Type': 'application/json',
+            'apiKey': os.getenv("SUPABASE_ANON_API_KEY"),
+            'Authorization': f'Bearer {os.getenv("SUPABASE_ANON_BEARER_TOKEN")}'
+        }
+        response = requests.post(api_url, json=payload, headers=headers)
+        console.print('[bold green]Response (Shipping Results Received):[/bold green]',response.content)
         
         if response.status_code == 200: # Successful execution (calculation happened)
             api_response_data = response.json()
             
             prompt = load_prompt('prompts/shipping_results_received.txt')
-            response = invoke_model_simple(prompt, f'User Message: {user_input}\nItem Data: {item_data}')
-            print('[bold green]Response (Shipping Results Received):[/bold green]',response.content)
+            response = invoke_model_simple(prompt, f'User Message: {user_input}\nShipping Results: {api_response_data}')
+            console.print('[bold green]Response (Shipping Results Received):[/bold green]',response.content)
             return response.content
         else: # There was an error
-            print('[bold red]Issues:[/bold red]',natural_language_error_message)
+            console.print('[bold red]Issues:[/bold red]',natural_language_error_message)
             return natural_language_error_message
 
 # Main func to run when script is loaded
 if __name__ == '__main__':
-    config = load_config()
-    
-    os.environ['OPENAI_API_KEY'] = os.getenv('OPENAI_API_KEY')
-    model = ChatOpenAI(model=config['openai']['model'])
-    
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000, reload=config['reload'])
+    uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=config['reload'])
